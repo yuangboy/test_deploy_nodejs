380e7d259fdfdc6a67a221e5e4815e7ba6e98a11
Albin_Mp <albinmpobo@gmail.com>
2025-05-13 00:24:04 +0200

    nex

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-console.log("oui");
+import express from "express";
+
+
+const app=express();
+
+
+app.get("/page",(req,res)=>{
+    res.send("Bienvenue dans la page")
+});
+
+const PORT=3000;
+app.listen(PORT, ()=>{console.log(`serveur lancé port: ${PORT}`);
+});
+
